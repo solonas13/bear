39d5995d40daf088a13c88b537d16b9e587483b0
Solon P Pissis <solonas13@gmail.com>
2015-03-19 10:19:10 +0000

    dataset update

--- exp3/exp3.raxml.sh
+++ exp3/exp3.raxml.sh
@@ -15,7 +15,12 @@
  rm RAxML_l*
  rm RAxML_p*
  rm RAxML_r*
+ ./raxmlHPC-PTHREADS-SSE3 -m GTRCAT -n 50.2500.35.cyc -p 12345 -s 50.2500.35.aln.cyc
+ rm RAxML_i*
+ rm RAxML_l*
+ rm RAxML_p*
+ rm RAxML_r*
 
- cat RAxML_bestTree.50.2500.35.true RAxML_bestTree.50.2500.35.bear RAxML_bestTree.50.2500.35.nobear > 50.2500.35.allTrees
+ cat RAxML_bestTree.50.2500.35.true RAxML_bestTree.50.2500.35.bear RAxML_bestTree.50.2500.35.nobear RAxML_bestTree.50.2500.35.cyc > 50.2500.35.allTrees
  ./raxmlHPC-PTHREADS-SSE3 -f r -z 50.2500.35.allTrees -m GTRCAT -n 50.2500.35
  rm RAxML_i*
--- exp3/exp3.run.sh
+++ exp3/exp3.run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../bear -a DNA -p 50.2500.35.rot.fas -k 45 -d 2 -w 45 -o 50.2500.35.bear.fas
+../bear -a DNA -p 50.2500.35.rot.fas -k 45 -d 1 -w 45 -o 50.2500.35.bear.fas
